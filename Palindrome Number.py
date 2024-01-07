1339ced4532792b451cde6f1aa83b13db5a4b0f7
Iris Karina Rodrigues <92535691+irisquintiliano@users.noreply.github.com>
2024-01-06 23:35:44 -0300

    Add files via upload

---
+++ Palindrome Number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        # Convert the integer to a string
+        str_x = str(x)
+        
+        # Compare the string with its reverse
+        return str_x == str_x[::-1]
+sol = Solution()
+
+
+result = sol.isPalindrome(121)
+print(result)
