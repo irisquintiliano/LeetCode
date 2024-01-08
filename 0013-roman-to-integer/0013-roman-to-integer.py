b77f74b98721be49bbdf6245f57168f93e032b7f
Iris Karina Rodrigues <92535691+irisquintiliano@users.noreply.github.com>
2024-01-07 23:58:13 -0300

    Time: 42 ms (19.18%), Space: 13.1 MB (99.40%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s):
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        total = 0
+        prev_value = 0
+        
+        for char in s:
+            current_value = roman_dict[char]
+            if current_value > prev_value:
+                total += current_value - 2 * prev_value
+            else:
+                total += current_value
+            prev_value = current_value
+        
+        return total
+
+# Test cases
+solution = Solution()
+print(solution.romanToInt("III"))        # Output: 3
+print(solution.romanToInt("LVIII"))      # Output: 58
+print(solution.romanToInt("MCMXCIV"))    # Output: 1994
